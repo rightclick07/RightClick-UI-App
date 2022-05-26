f0efefc87579fe56908d042f6026aced845363ef
unknown <heyravishankar07@gmail.com>
2022-05-27 01:40:36 +0530

    added comming soon text for upcoming course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
+
+declare var gtag:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'right-click';
+  constructor(router: Router) {
+    const navEndEvent = router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    );
+    navEndEvent.subscribe((event:any) => {
+      gtag('config', 'G-6QJNNYL16D',{
+        'page_path': event.urlAfterRedirects
+      });
+    });
+  
+  }
 }
