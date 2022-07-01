42627aee62eb8f5c854ce01c8d729fd68a9838db
unknown <heyravishankar07@gmail.com>
2022-07-01 14:10:57 +0530

    search enhanced & course menu added with more courses

--- src/app/core/main-nav/main-nav.component.ts
+++ src/app/core/main-nav/main-nav.component.ts
@@ -30,18 +30,136 @@ export class MainNavComponent {
   showSocialForweb=true;
   myControl = new FormControl();
   courses = [
+    
+    {
+      title:"Artificial Intelligence",
+      routeUrl: 'ai',
+    },
     {
       title:"Angular",
       routeUrl:"angular"
     },
     {
       title:"Angular Material",
-      routeUrl:"angular-material"
+      routeUrl: 'angular-material',
+
+    },
+    {
+      title:"AWS",
+      routeUrl: 'aws'
+    },
+    {
+      title:"Blockchain",
+      routeUrl: 'blockchain'
+    },
+    {
+      title:"C Programming",
+      routeUrl: 'c'
+      
+    },
+    {
+      title:"C++ Programming",
+      routeUrl: 'c++'
+    },
+    {
+      title:"Computer Network",
+      routeUrl: 'cn'
+    },
+    {
+      title:"Computer Organisation",
+      routeUrl: 'coa'
+    },
+    {
+      title:"Compiler Design",
+      routeUrl: 'compiler-design'
+    },
+    {
+      title:"CSS",
+      routeUrl: 'css'
+    },
+    {
+      title:"Cyber Security",
+      routeUrl: 'cyber-security'
+    },
+    {
+      title:"Data Science",
+      routeUrl: 'data-science'
+    },
+    {
+      title:"C Programming",
+      routeUrl: 'dbms',
+    },
+    {
+      title:"Discrete Maths",
+      routeUrl: 'discrete',
+    },
+    {
+      title:"Data Structure and Algorithm",
+      routeUrl: 'dsa',
+    },
+    {
+      title:"Flutter",
+      routeUrl: 'flutter',
     },
     {
       title:"HTML",
-      routeUrl:"html"
-    }
+      routeUrl: 'html',
+    },
+    {
+      title:"Java Programming",
+      routeUrl: 'java',
+      
+    },
+    {
+      title:"Javascript",
+      routeUrl: 'javascript', 
+    },
+    {
+      title:"Kubernetes",
+      routeUrl: 'kubernetes'
+    },
+    {
+      title:"Machine Learning",
+      routeUrl: 'ml'
+    },
+    {
+      title:"Operating System",
+      routeUrl: 'os' 
+    },
+    {
+      title:"Python",
+      routeUrl: 'python'
+    },
+    {
+      title:"React",
+      routeUrl: 'react',
+    },
+    {
+      title:"SEO",
+      routeUrl: 'seo',
+    },
+    {
+      title:"Software Engineering",
+      routeUrl: 'software-engineering',
+    },
+    {
+      title:"Spring-Boot",
+      routeUrl: 'springboot',
+    },
+    {
+      title:"SQL",
+      routeUrl: 'sql', 
+    },
+    {
+      title:"System Design",
+      routeUrl: 'system-design',
+    },
+    {
+      title:"TypeScript",
+      routeUrl: 'typescript',
+      
+    },
+  
     ];
     companyMenuList = [
       {
@@ -72,7 +190,13 @@ export class MainNavComponent {
           icon:"exit_to_app"
         }
         ];
-  options: string[] = ['Angular', 'Angular Material', 'HTML'];
+  options: string[] = ['Artificial Intelligence','Angular','Angular Material','AWS',
+  'Blockchain','C Programming','C++ Programming','Computer Network','Computer Organisation',
+  'Compiler Design','CSS','Cyber Security','Data Science','C Programming',
+  'Discrete Maths','Data Structure and Algorithm','Flutter','HTML',
+  'Java Programming','Javascript','Kubernetes','Machine Learning',
+  'Operating System','Python','React','SEO','Software Engineering',
+  'Spring-Boot','SQL','System Design','TypeScript'];
   filteredOptions: Observable<string[]> = new Observable<string[]>();
   ngOnInit() {
     this.filteredOptions = this.myControl.valueChanges.pipe(
